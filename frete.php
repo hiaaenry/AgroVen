0fc062ad9c72b0dc5bfde9143a8633c8b262f0cf
Luanjoss <luanmicro890@gmail.com>
2021-12-10 22:17:03 -0300

    Mensagem de confirmação dos dados do frete

--- frete.php
+++ frete.php
@@ -11,9 +11,9 @@ $pont_ref = $_POST['ponto'];
 $stmt = $conectar->prepare("INSERT INTO finalizar_compra(rua,numero,complemento,cidade,pont_ref) VALUES(?,?,?,?,?)");
 $stmt->execute([$rua,$numero,$complemento,$cidade,$pont_ref]);
 
-header("Location:index.php");
+//header("Location:index.php");
 
-echo "<script>window.location='registro.php';alert('Cadastro realizado com sucesso!');</script>"
+echo "<script>window.location='finalizarcompra.php';alert('Cadastro do envio do frete realizado com sucesso!');</script>"
 
 ?>
 
